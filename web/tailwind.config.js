c4667d1600b9b6f4869bb558fd031e3710ded57b
yumekiti <yumekiti120440@gmail.com>
2023-04-04 00:37:13 +0900

    `add` accent color

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#09abec',
+      },
+    },
   },
   plugins: [],
 }
